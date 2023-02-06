9c44721fed5fbe7c35ffd5c2ea9057b50b481608
Jake <daehyun-kyung@nhn-commerce.com>
2023-02-06 15:54:20 +0900

    factorial recursive

--- python/factorial.py
+++ python/factorial.py
@@ -2,7 +2,6 @@ def factorial(n):
     f = 1
     for i in range(1, n+1):
         f = f * i
-        print(f)
     return f
 
 print(factorial(5))
@@ -14,3 +13,12 @@ f = 6 * 4 = 24
 f = 24 * 5 = 120
 output : 120
 '''
+
+def factorial_recursive(n):
+    if n <= 1:
+        return 1
+    return n * factorial_recursive(n-1)
+
+print(factorial_recursive(3))
+
+# output : 3 * 2 * 1 = 6
