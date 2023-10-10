7869f0b1f2806c06d85e245e3c838c181589591e
Daehyun Kyung <cokolavel@gmail.com>
2023-10-10 13:33:53 +0900

    사칙연산

---
+++ PHP/basic.php
@@ -0,0 +1,7 @@
+<?php
+    fscanf(STDIN,"%d %d", $a,$b);
+    fprintf(STDOUT,"%d\n", $a+$b);
+    fprintf(STDOUT,"%d\n", $a-$b);
+    fprintf(STDOUT,"%d\n", $a*$b);
+    fprintf(STDOUT,"%d\n", $a/$b);
+    fprintf(STDOUT,"%d", $a%$b);
---
+++ PHP/divide.php
@@ -0,0 +1,3 @@
+<?php
+    fscanf(STDIN,"%d %d", $a,$b);
+    fprintf(STDOUT,"%.9f", (double)$a/(double)$b);
