a26144cad2d694b8a0a08e1c629dc082dd6e6acb
Daehyun Kyung <cokolavel@gmail.com>
2023-10-22 18:56:10 +0900

    modify

--- PHP/18108.php
+++
@@ -1,6 +0,0 @@
-/**
-* 불기 연도 -> 서기 연도 출력
-*/
-<?php
-    fscanf(STDIN,"%d", $a);
-    fprintf(STDOUT,"%d", $a-543);
---
+++ PHP/Etc/18108.php
@@ -0,0 +1,6 @@
+<?php
+/**
+ * 불기 연도 -> 서기 연도 출력
+ */
+fscanf(STDIN,"%d", $a);
+fprintf(STDOUT,"%d", $a-543);
